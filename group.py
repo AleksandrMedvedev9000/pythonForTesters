d7a67de66e7ce07f7037d7e9e61c767c5c5c8589
Aleksandr Serikov <52409478+Aserikov@users.noreply.github.com>
2019-11-21 01:56:37 +0300

    Выделены вспомогательные методы и классы

---
+++ group.py
@@ -0,0 +1,6 @@
+class Group:
+
+    def __init__(self, table_name, table_header, table_footer):
+        self.table_name = table_name
+        self.table_header = table_header
+        self.table_footer = table_footer
--- new_group.py
+++ new_group.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
+
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
+from group import Group
 
 class NewGroup(unittest.TestCase):
     def setUp(self):
@@ -14,28 +13,52 @@ class NewGroup(unittest.TestCase):
     
     def test_new_group(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-        wd.find_element_by_link_text("groups").click()
+        self.open_main_page(wd)
+        self.log_in(wd, username="admin", user_password="secret")
+        self.open_group_page(wd)
+        self.create_new_group(wd, Group(table_name="Preved!", table_header="Bonjour!", table_footer="Zdarova!"))
+        self.open_group_page(wd)
+        self.log_out(wd)
+
+    def test_new_empty_group(self):
+        wd = self.wd
+        self.open_main_page(wd)
+        self.log_in(wd, username="admin", user_password="secret")
+        self.open_group_page(wd)
+        self.create_new_group(wd, Group(table_name="", table_header="", table_footer=""))
+        self.open_group_page(wd)
+        self.log_out(wd)
+
+    def log_out(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def create_new_group(self, wd, group):
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("Preved!")
+        wd.find_element_by_name("group_name").send_keys(group.table_name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("Bonjour!")
+        wd.find_element_by_name("group_header").send_keys(group.table_header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("Zdarova!")
+        wd.find_element_by_name("group_footer").send_keys(group.table_footer)
         wd.find_element_by_name("submit").click()
+
+    def open_group_page(self, wd):
         wd.find_element_by_link_text("groups").click()
-        wd.find_element_by_link_text("Logout").click()
-    
+
+    def log_in(self, wd, username, user_password):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(user_password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_main_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
