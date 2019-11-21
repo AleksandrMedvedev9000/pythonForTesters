dfbee7f5670a3f8e310e4779c302eb384ad6db94
Aleksandr Serikov <52409478+Aserikov@users.noreply.github.com>
2019-11-21 03:31:41 +0300

    Тест создания контакта

---
+++ contact.py
@@ -0,0 +1,25 @@
+class Contact:
+
+    def __init__(self, first_name, middle_name, last_name, nickname, title, company, address, home_phone, mobile_phone,
+                             work_phone, fax, email, bday, bmonth, byear, aday, amonth, ayear, address2, phone2, notes):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.fax = fax
+        self.email = email
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
