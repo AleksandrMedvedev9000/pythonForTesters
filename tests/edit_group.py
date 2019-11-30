03ecd675cd7bd77d49d1c9e5579a6ced28c195c1
Aleksandr Serikov <52409478+Aserikov@users.noreply.github.com>
2019-11-30 07:42:46 +0300

    Revert "Выделены вспомогательные методы для заполнения полей"
    This reverts commit 1cc2889c942d62f79af328382bc7dbab99dc0c98.

--- tests/edit_group.py
+++ tests/edit_group.py
@@ -1,14 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 
-
-def test_edit_first_group_name(app):
+def test_delete_first_group(app):
     app.session.log_in(username="admin", user_password="secret")
-    app.group.edit_first(Group(table_name="qqq"))
-    app.session.log_out()
-
-
-def test_edit_first_group_header(app):
-    app.session.log_in(username="admin", user_password="secret")
-    app.group.edit_first(Group(table_header="222"))
+    app.group.edit_first(Group(table_name="111", table_header="222", table_footer="333"))
     app.session.log_out()
