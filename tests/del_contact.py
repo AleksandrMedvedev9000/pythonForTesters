ee410e83e5ef822db284d4cee0c9e2313587a0ab
Aleksandr Serikov <52409478+Aserikov@users.noreply.github.com>
2019-11-30 07:49:23 +0300

    Revert "Тесты на создание, модификацию и удаление групп и контактов"
    This reverts commit e8863930ede92402c8089e9bc3a7efdb3cb1f254.

--- tests/del_contact.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-def test_delete_first_group(app):
-    app.session.log_in(username="admin", user_password="secret")
-    app.contact.delete_first()
-    app.session.log_out()
