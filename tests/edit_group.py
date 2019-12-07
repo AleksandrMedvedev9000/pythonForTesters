4473671ff83a4662e5a8646ae36d40d5f4cbd5d3
Aleksandr Serikov <52409478+Aserikov@users.noreply.github.com>
2019-12-07 19:03:59 +0300

    В фикстуру добавлена проверка на запуск

--- tests/edit_group.py
+++ tests/edit_group.py
@@ -3,12 +3,8 @@ from model.group import Group
 
 
 def test_edit_first_group_name(app):
-    app.session.log_in(username="admin", user_password="secret")
     app.group.edit_first(Group(table_name="qqq"))
-    app.session.log_out()
 
 
 def test_edit_first_group_header(app):
-    app.session.log_in(username="admin", user_password="secret")
     app.group.edit_first(Group(table_header="222"))
-    app.session.log_out()
--- tests/new_group.py
+++ tests/new_group.py
@@ -3,12 +3,8 @@ from model.group import Group
 
 
 def test_new_group(app):
-    app.session.log_in(username="admin", user_password="secret")
     app.group.create_new(Group(table_name="Preved!", table_header="Bonjour!", table_footer="Zdarova!"))
-    app.session.log_out()
 
 
 def test_new_empty_group(app):
-    app.session.log_in(username="admin", user_password="secret")
     app.group.create_new(Group(table_name="", table_header="", table_footer=""))
-    app.session.log_out()
