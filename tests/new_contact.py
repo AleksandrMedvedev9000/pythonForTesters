aff9a36c08316f5fabab4f51e8384b63a7b7f79b
Aleksandr Serikov <52409478+Aserikov@users.noreply.github.com>
2019-12-07 19:09:52 +0300

    Revert "Revert "В фикстуру добавлена проверка на запуск""
    This reverts commit 8ca2fc39e00dd0416a8da4baea5ddeecdb655eeb.

--- tests/new_contact.py
+++ tests/new_contact.py
@@ -2,10 +2,8 @@
 from model.contact import Contact
 
 def test_new_contact(app):
-    app.session.log_in(username="admin", user_password="secret")
     app.contact.add_new(Contact(first_name="Preved!", middle_name="Bonjour!", last_name="Zdarova!", nickname="Gutentag!", title="Bonapetit",
                                 company="Carambol", address="Bodybuild", home_phone="Copperwire", mobile_phone="Budapest",
                                 work_phone="Brandenburg", fax="Brontozavr", email="Kremdlyaruk@mail.com", bday="10", bmonth="May",
                                 byear="1970", aday="10", amonth="May", ayear="2020", address2="Bodyguard", phone2="Cyberpunk",
                                 notes="Hello!"))
-    app.session.log_out()
