1d49be91f346407129c90ca09581765ad8cc6864
Aleksandr Serikov <52409478+Aserikov@users.noreply.github.com>
2019-12-12 12:06:51 +0300

    Обеспечено выполнение предусловий для удаления и модификации групп

--- fixture/group.py
+++ fixture/group.py
@@ -10,7 +10,6 @@ class GroupHelper:
         self.fill_group_fields(group)
         wd.find_element_by_name("submit").click()
         self.open_group_page()
-        wd.find_element_by_link_text("Logout").click()
 
     def fill_group_fields(self, group):
         self.check_and_type("group_name", group.table_name)
@@ -47,3 +46,8 @@ class GroupHelper:
         self.fill_group_fields(group)
         wd.find_element_by_name("update").click()
         self.open_group_page()
+
+    def count(self):
+        wd = self.app.wd
+        self.open_group_page()
+        return len(wd.find_elements_by_name("selected[]"))
--- tests/edit_group.py
+++ tests/edit_group.py
@@ -3,8 +3,12 @@ from model.group import Group
 
 
 def test_edit_first_group_name(app):
+    if app.group.count() == 0:
+        app.group.create_new(Group(table_name="", table_header="", table_footer=""))
     app.group.edit_first(Group(table_name="qqq"))
 
 
 def test_edit_first_group_header(app):
+    if app.group.count() == 0:
+        app.group.create_new(Group(table_name="", table_header="", table_footer=""))
     app.group.edit_first(Group(table_header="222"))
